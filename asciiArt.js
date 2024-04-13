b1697c093da7f386726c1edf0141caeb7b270979
Jimena Noa <noaguevg@oregonstate.edu>
2024-04-13 13:40:44 -0700

    added code to create ascii art

--- asciiArt.js
+++ asciiArt.js
@@ -1,5 +1,14 @@
 import { argv } from 'node:process';
+import figlet from 'figlet';
 
 // print input
 console.log(argv[2]);
 
+figlet(argv[2], function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
